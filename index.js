e1c07096fa77a3c688a4d029ec6286fbbebde302
Kirill <kiev.programmer@gmail.com>
2017-09-19 16:16:33 +0300

    mongo storage

--- index.js
+++ index.js
@@ -1,58 +1,32 @@
 /* eslint-env node */
-const fs = require('fs')
 const TelegramBot = require('node-telegram-bot-api')
-const stateLocation = './data/state.json'
-const dataFolder = './data'
-const chatsFolder = './data/chats'
-const getChatFolder = chatId => `${chatsFolder}/${chatId}`
+const {MongoClient} = require('mongodb')
+const {okTexts} = require('./src/texts')
 
-const telegramBotToken = process.argv[2]
-if (!telegramBotToken) {
-  throw new Error('Usage: node index.js YOUR_TELEGRAM_BOT_TOKEN')
+const url = 'mongodb://localhost:27017/pmbot_v01'
+
+const connection = async function () {
+  return MongoClient.connect(url)
 }
 
-let state = loadState() || getInitialState()
+const start = async function () {
+  const db = await connection()
+  console.log('Connected correctly to server')
+  const telegramBotToken = process.argv[2]
+  if (!telegramBotToken) {
+    throw new Error('Usage: node index.js YOUR_TELEGRAM_BOT_TOKEN')
+  }
+  const bot = new TelegramBot(telegramBotToken, {polling: true})
+  console.log('Bot started')
+  return {db, bot}
+}
 
-const bot = new TelegramBot(telegramBotToken, {polling: true})
-console.log('Started')
-
-bot.on('polling_error', (error) => {
-  console.log(error)
+start().then(({db, bot}) => {
+  bot.on('message', (msg) => onMessage(bot, db, msg).then())
 })
 
-bot.on('message', (msg) => {
-  const chat = getChat(state, msg.chat.id)
-  dumpMsg(chat, msg.text)
-})
-
-function loadState () {
-  if (!fs.existsSync(stateLocation)) {
-    return null
-  }
-  return JSON.parse(fs.readFileSync(stateLocation, {encoding: 'utf8'}))
-}
-function saveState (state) {
-  fs.writeFileSync(stateLocation, JSON.stringify(state))
-  return state
-}
-function getInitialState () {
-  try { fs.statSync(dataFolder) } catch (e) { fs.mkdirSync(dataFolder) }
-  try { fs.statSync(chatsFolder) } catch (e) { fs.mkdirSync(chatsFolder) }
-  return {chats: []}
+async function onMessage (bot, db, msg) {
+  await db.collection('messages').insertOne(msg)
+  bot.sendMessage(msg.chat.id, okTexts[Math.floor(Math.random() * okTexts.length)])
 }
 
-function getChat (state, chatId) {
-  let chat = state.chats.find(c => c.id === chatId)
-  if (!chat) {
-    chat = {id: chatId}
-    try { fs.statSync(getChatFolder(chatId)) } catch (e) { fs.mkdirSync(getChatFolder(chatId)) }
-    state.chats.push(chat)
-    saveState(state)
-  }
-  return chat
-}
-
-function dumpMsg (chat, msg) {
-  const now = new Date().toISOString()
-  fs.appendFileSync(`${getChatFolder(chat.id)}/${now.substr(0, 10)}`, `${now} ${(msg || '').replace(/\n/g, '\\n')}\n`)
-}
---
+++ src/texts.js
@@ -0,0 +1,4 @@
+exports.okTexts = [
+  'ok', 'got it', 'affirmative', 'roger', 'roger that', 'good', 'fine', 'right', 'all right', 'great', 'sounds good',
+  'no problem', 'sure', 'as you  say', 'ACK', 'all righty', 'no prob', 'hell yeah', 'yeah', 'i\'m with you', 'cool'
+]
